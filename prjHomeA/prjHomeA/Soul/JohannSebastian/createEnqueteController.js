70fb0a072e53f7c50eec6d06074d2210ca1895d8
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-09-08 19:01:56 -0300

    Página de Reservas - Incompleta

--- prjHomeA/prjHomeA/Soul/JohannSebastian/createEnqueteController.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/createEnqueteController.js
@@ -1,15 +1,34 @@
 ﻿class EnqueteController{
     constructor(){
+        this._questionNumber = 0;
+        this.Init();
         
     }
 
     Init(){
-        
+        var btnNewPergunta = document.querySelector("#NovaPergunta");
+        this.addEventListenerAll(btnNewPergunta, 'click drag dblclick',e=>{
+            let Question = document.createElement('div');
+            Question.classList.add("Perguntados");
+            Question.classList.add("DIO");
+            Question.id = "Question" + this._questionNumber.toString();
+            this._questionNumber++;
+            Question.innerHTML = "RENATO GARCIA É UM DEUS";
+            if (condition) {
+                
+            }
+            document.querySelector("#n1").appendChild(Question);
+
+        })
 
     }
 
-    addEventListenerAll(){
-
+    addEventListenerAll(Element, Events,Functional){
+        let Evento = Events.split(' ');
+        Evento.forEach(Splito => {
+            Element.addEventListener(Splito,Functional);
+            
+        });
 
     }
 

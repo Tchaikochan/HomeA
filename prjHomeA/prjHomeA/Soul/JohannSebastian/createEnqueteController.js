555410a10ceeba8a6ff4a6b34822e7f54896f65a
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-09-08 20:07:15 -0300

    Página de Reservas - Incompleta

--- prjHomeA/prjHomeA/Soul/JohannSebastian/createEnqueteController.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/createEnqueteController.js
@@ -1,23 +1,57 @@
 ﻿class EnqueteController{
     constructor(){
-        this._questionNumber = 0;
+        this._questionNumber = 2;
         this.Init();
         
+    }
+
+    verifPar(Namba){
+        if (Namba % 2 == 0) {
+            return false;
+
+        } else{
+            return true;
+
+        }
+
     }
 
     Init(){
         var btnNewPergunta = document.querySelector("#NovaPergunta");
         this.addEventListenerAll(btnNewPergunta, 'click drag dblclick',e=>{
+            btnNewPergunta.parentNode.removeChild(btnNewPergunta);
             let Question = document.createElement('div');
-            Question.classList.add("Perguntados");
-            Question.classList.add("DIO");
-            Question.id = "Question" + this._questionNumber.toString();
-            this._questionNumber++;
-            Question.innerHTML = "RENATO GARCIA É UM DEUS";
-            if (condition) {
-                
+            if (this.verifPar(this._questionNumber)) {
+                Question.classList.add("bgBlanc");
+                Question.classList.add("Blanc");
+
+            } else{
+                Question.classList.add("bgNoire");
+                Question.classList.add("Noire"); 
+
             }
-            document.querySelector("#n1").appendChild(Question);
+            Question.id = "L" + this._questionNumber.toString();
+            
+            this._questionNumber++;
+            Question.innerHTML = `
+            <div class="questionBA">
+            <input type="text" class="generalQuestionsText" id="formTitleEnquete" placeholder="Digite Sua Pergunta">
+                <i class="material-icons" style="font-size: 80px; padding: 40px;">create</i>
+                <br /><br /><br />
+            </div>
+            `;
+                document.querySelector("#formCreateFullEnquete").appendChild(Question);
+                document.querySelector("#formCreateFullEnquete").appendChild(btnNewPergunta);
+                
+                if (this.verifPar(this._questionNumber)) {
+                    document.body.style.backgroundColor = "#86E9BD";
+                    btnNewPergunta.style.backgroundColor = "#5BF8E4";
+
+                } else{
+                    document.body.style.backgroundColor = "#5BF8E4";
+                    btnNewPergunta.style.backgroundColor = "#86E9BD";
+
+                }
 
         })
 

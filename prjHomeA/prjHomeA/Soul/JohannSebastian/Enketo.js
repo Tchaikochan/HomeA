6ae6f547bcedcd386095b66bbad6aecbfdf7391e
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-10-26 18:39:25 -0300

    Enquete

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/Enketo.js
@@ -0,0 +1,7 @@
+let Former = document.querySelector("#Subete");
+Former.addEventListener("submit",e=>{
+    e.preventDefault();
+    let Title = (Former.children[0].children[1].value);
+    let DS = (Former.children[1].children[1].value);
+
+});

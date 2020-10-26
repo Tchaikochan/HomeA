90b6ea0a8beb0da3b8724dc0af6611e9d6d9f2f3
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-10-26 14:32:31 -0300

    Reservas

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/Reservoir.js
@@ -0,0 +1,9 @@
+﻿document.querySelector("#Subete").addEventListener("submit",e=>{
+    e.preventDefault();
+    let Rooms = document.querySelectorAll(".container");
+    [...Rooms].forEach(element=>{
+        console.log(element.children[1].checked);
+        
+    });
+
+});

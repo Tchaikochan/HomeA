1ec03be7459aeeb75f4a196afda43ac47a2dc115
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-10-27 13:14:01 -0300

    Enquete

--- prjHomeA/prjHomeA/Soul/JohannSebastian/Enketo.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/Enketo.js
@@ -1,7 +1,21 @@
+window.onload = initPage;
+
+function initPage(){
+    
+
+}
+
 let Former = document.querySelector("#Subete");
 Former.addEventListener("submit",e=>{
     e.preventDefault();
     let Title = (Former.children[0].children[1].value);
     let DS = (Former.children[1].children[1].value);
+    let Questions = Former.children[2];
+    [...Questions.children].forEach(element => {
+        if (element.classList.contains("radio")){
+            console.log(element);
+            
+        }
+    });
 
 });

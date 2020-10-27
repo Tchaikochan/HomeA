4de1595569c2c08e25db83d4966098b2a34707d2
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-10-27 15:43:42 -0300

    Enquete

--- prjHomeA/prjHomeA/Soul/JohannSebastian/Enketo.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/Enketo.js
@@ -2,6 +2,7 @@ window.onload = initPage;
 var Former = document.querySelector("#Subete");
 var Questions = Former.children[2];
 var Altera = [];
+var Block = true;
 
 function initPage(){
     Questions.innerHTML +=
@@ -10,9 +11,9 @@ function initPage(){
 
     <div class="radio">
         <h4>Pergunta 1</h4>
-        <label for="1"><h5>Alternativa 1</h5></label>
+        <h5>Alternativa 1</h5>
         <input type="radio" id="1" name="1" value="">
-        <label for="2"><h5>Alternativa 2</h5></label>
+        <h5>Alternativa 2</h5>
         <input type="radio" id="2" name="2" value="">   
      </div>
 
@@ -20,9 +21,9 @@ function initPage(){
 
     <div class="radio">
         <h4>Pergunta 2</h4>
-        <label for="1"><h5>Alternativa 1</h5></label>
+        <h5>Alternativa 1</h5>
         <input type="radio" id="1" name="1" value="">
-        <label for="2"><h5>Alternativa 2</h5></label>
+        <h5>Alternativa 2</h5>
         <input type="radio" id="2" name="2" value="">
     </div>
 
@@ -36,7 +37,12 @@ function EditName() {
     let C = 0;
     [...Questions.children].forEach(element => {
         if (element.classList.contains("radio")){
-            Editer(element);
+            V = 0;
+            while (element.children[V] != null) {
+                Editer(element,V);
+                V++;
+
+            }
             C++;
         }
     });
@@ -50,32 +56,34 @@ Former.addEventListener("submit",e=>{
 
 });
 
-function Editer(element){
-    element.children[0].addEventListener("click",e=>{
-        var CB = document.createElement("input");
-        var BTN = document.createElement("div");
-        BTN.innerHTML = 
-        `
-        <hr />   
-        <button>Salvar Alterações</button>
-        `;
-        CB.type = "text";
-        CB.id = "ChangeASK"
-        console.log(element);
-        BTN.id = `BTN`;
-        let Kill = e.target;
-        let NodeParent = Kill.parentNode;
-        CB.value = Kill.innerHTML;
-        Kill.parentNode.replaceChild(CB,Kill);
-        element.appendChild(BTN);
-        BTN.children[1].addEventListener("click",f=>{
-            BTN.remove();        
-            if(CB.value == "" || CB.value == null) CB.value = "Hey! Você não escreveu nada."
-            Kill.innerHTML = CB.value;
-            NodeParent.replaceChild(Kill,CB);
-        
-        });
+function Editer(element,V){
+    if (element.children[V].type != "radio") {
+        element.children[V].addEventListener("click",e=>{
+            var CB = document.createElement("input");
+            var BTN = document.createElement("div");
+            BTN.innerHTML = 
+            `
+            <hr />   
+            <button>Salvar Alterações</button>
+            `;
+            CB.type = "text";
+            CB.id = "ChangeASK";
+            console.log(element);
+            BTN.id = `BTN`;
+            let Kill = e.target;
+            let NodeParent = Kill.parentNode;
+            CB.value = Kill.innerHTML;
+            Kill.parentNode.replaceChild(CB,Kill);
+            element.appendChild(BTN);
+            BTN.children[1].addEventListener("click",f=>{
+                BTN.remove();        
+                if(CB.value == "" || CB.value == null) CB.value = "Hey! Você não escreveu nada."
+                Kill.innerHTML = CB.value;
+                NodeParent.replaceChild(Kill,CB);
 
-    });
+            });
+
+        });
+    }
 
 }

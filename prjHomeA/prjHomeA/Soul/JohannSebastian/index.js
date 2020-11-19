5193a5e6a725a1e4e56a30871d03458840c9cf8b
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-19 15:38:08 -0300

    Login

--- prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
@@ -2,7 +2,7 @@ document.querySelector("#FormLog").addEventListener("submit",e=>{
     e.preventDefault();
     let Email = document.querySelector("#MAIL");
     let Pass = document.querySelector("#PASSWORD");
-    alert("ra0");   
+    alert("ra0");
     $.post("index.aspx",{e:Email,p:Pass,c:true},function(callbacku){
         alert("SOLTURA DO LULA!");
         
@@ -15,8 +15,11 @@ document.querySelector("#InsertLog").addEventListener("submit",e=>{
     let Name = document.querySelector("#NAMELOG");
     let Email = document.querySelector("#MAILLOG");
     let Pass = document.querySelector("#PASSLOG");
-    alert("ra0");   
-    $.post("index.aspx",{n:Name,e:Email,p:Pass,c:false},function(callbacku){
+    let Condominio = document.querySelector("#CONDONLOG");
+    let Apartamento = document.querySelector("#APARTLOG");
+    let Funccao = document.querySelector("#SELECTO").options[document.querySelector("#SELECTO").selectedIndex].value;
+    alert(Funccao);   
+    $.post("index.aspx",{n:Name,e:Email,p:Pass,co:Condominio,a:Apartamento,f:Funccao,c:false},function(callbacku){
         alert("FORA THEMER!");
         
     });

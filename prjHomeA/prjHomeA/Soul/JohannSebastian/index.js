5349b29da7b93450fd6c88f725e9c6ce6db5ea69
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-19 14:50:20 -0300

    Login

--- prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
@@ -3,8 +3,21 @@ document.querySelector("#FormLog").addEventListener("submit",e=>{
     let Email = document.querySelector("#MAIL");
     let Pass = document.querySelector("#PASSWORD");
     alert("ra0");   
-    $.post("index.aspx",{e:Email,p:Pass,},function(callbacku){
+    $.post("index.aspx",{e:Email,p:Pass,c:true},function(callbacku){
         alert("SOLTURA DO LULA!");
+        
+    });
+
+})
+
+document.querySelector("#InsertLog").addEventListener("submit",e=>{
+    e.preventDefault();
+    let Name = document.querySelector("#NAMELOG");
+    let Email = document.querySelector("#MAILLOG");
+    let Pass = document.querySelector("#PASSLOG");
+    alert("ra0");   
+    $.post("index.aspx",{n:Name,e:Email,p:Pass,c:false},function(callbacku){
+        alert("FORA THEMER!");
         
     });
 
--- prjHomeA/prjHomeA/Soul/Pages/index.aspx.cs
+++ prjHomeA/prjHomeA/Soul/Pages/index.aspx.cs
@@ -11,6 +11,17 @@ namespace prjHomeA.Soul.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string isRunning = Request["c"].ToString();
+            if (isRunning == "false")
+            {
+                string Name = Request["n"];
+                string Email = Request["e"];
+                string Password = Request["p"];
+
+
+            } else {
+
+            }
 
         }
     }

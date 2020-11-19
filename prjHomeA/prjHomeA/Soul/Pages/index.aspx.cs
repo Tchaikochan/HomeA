c1a011605aca194d2a0e7f8baa9d8b70a0dad043
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-19 15:52:59 -0300

    Login

--- prjHomeA/prjHomeA/Soul/Pages/index.aspx.cs
+++ prjHomeA/prjHomeA/Soul/Pages/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
 namespace prjHomeA.Soul.Pages
 {
@@ -11,12 +12,62 @@ namespace prjHomeA.Soul.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataBase Banco = new DataBase();
+
             string isRunning = Request["c"].ToString();
             if (isRunning == "false")
             {
                 string Name = Request["n"];
+                if (Name == "" || Name == null)
+                {
+                    return;
+
+                }
                 string Email = Request["e"];
+                if (Email == "" || Email == null)
+                {
+                    return;
+
+                }
+
                 string Password = Request["p"];
+                if (Password == "" || Password == null)
+                {
+                    return;
+
+                }
+                string Condominio = Request["co"];
+                if (Condominio == "" || Condominio == null)
+                {
+                    return;
+
+                }
+                string Apartamento = Request["a"];
+                if (Apartamento == "" || Apartamento == null)
+                {
+                    return;
+
+                }
+                string Funccao = Request["f"];
+                if (Funccao == "" || Funccao == null)
+                {
+                    return;
+
+                }
+
+                int Counter = 0;
+                Banco.openBar("localhost", "root", "root", "HomeA");
+                Banco.getCommand("SELECT * from Usuario");
+                while (Banco.Selected.Read())
+                {
+                    Counter++;
+
+                }
+
+                Banco.Refresh();
+                Banco.openBar();
+                Banco.setCommand("INSERT INTO Usuario VALUES('');");
+                Banco.Refresh();
 
 
             } else {

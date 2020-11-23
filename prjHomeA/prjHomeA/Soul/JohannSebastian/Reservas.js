cc70ddf72d56f34b823cc0131be8d8320528b6de
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-23 14:08:19 -0300

    Reefer

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/Reservas.js
@@ -0,0 +1,27 @@
+﻿window.onload = initPage;
+var isRunning = false;
+var Condominio = "1";
+
+function initPage(){
+    $.post("reservar.aspx",{checker:isRunning,c:Condominio},function(callbacku){
+        isRunning = true;
+        let former = document.createElement("div");
+        let Splinter = callbacku.split("#");
+        Splinter.pop();
+        Splinter.forEach(element => {
+            let Master = element.split("$");
+            former.innerHTML += 
+            `
+            <label class="container"><h4 class="h4r">${Master[0]}</h4>
+                <input type="checkbox" value="${Master[1]}">
+                <span class="checkmark"></span>
+            </label>
+            `
+
+        });
+        
+        document.querySelector("#Subete").appendChild(former);
+        
+    });
+
+}

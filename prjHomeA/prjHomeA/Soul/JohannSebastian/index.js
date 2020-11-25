d035e88fb7ef11552aa6c447263d9977bbcab603
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-25 19:16:34 -0300

    Container

--- prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/index.js
@@ -7,16 +7,16 @@ document.querySelector("#FormLog").addEventListener("submit",e=>{
         window.sessionStorage.setItem('Condominio', callbacku.substring(1,callbacku.indexOf("&")));
         
         if (callbacku.substring(0,1) == "0") {
-            //window.location.href = "Shome.html";
+            window.location.href = "Shome.html";
 
         } else if (callbacku.substring(0,1) == "1") {
-            //window.location.href = "Shome.html";
+            window.location.href = "Shome.html";
             
         } else if (callbacku.substring(0,1) == "2") {
-            //window.location.href = "Shome.html";
+            window.location.href = "Shome.html";
             
         } else if (callbacku.substring(0,1) == "3") {
-            //window.location.href = "Fhome.html";
+            window.location.href = "Fhome.html";
             
         } else {
             alert("Login Inválido");

1911ff3c576a230da38459a15709547671cd0524
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-25 23:37:58 -0300

    Container

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/reclamaki.js
@@ -0,0 +1,9 @@
+document.querySelector("#Kimochi").addEventListener("submit",e=>{
+    let Title = document.querySelector("#enquete");
+    let DS = document.querySelector("#msg");
+    $.post("index.aspx",{D:DS,T:Title},function(callbacku){        
+
+
+    });
+
+});

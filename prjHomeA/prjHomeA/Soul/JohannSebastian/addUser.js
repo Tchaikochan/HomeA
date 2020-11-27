98d2375c375c36327d2ea45f59d5c7b3f32e3110
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-27 16:49:33 -0300

    Container

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/addUser.js
@@ -0,0 +1,10 @@
+window.onload = initPage;
+
+
+function initPage(){
+    $.post("Reservas.aspx",{checker:isRunning,c:Condominio},function(Carubacku){
+
+
+    });
+
+}
---
+++ prjHomeA/prjHomeA/Soul/Pages/addUser.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="addUser.aspx.cs" Inherits="prjHomeA.Soul.Pages.addUser" %>

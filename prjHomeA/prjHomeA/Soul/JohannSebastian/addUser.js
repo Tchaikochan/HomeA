6998bf94457338414535dcff9c34a267374500b6
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-27 17:01:20 -0300

    Container

--- prjHomeA/prjHomeA/Soul/JohannSebastian/addUser.js
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/addUser.js
@@ -3,7 +3,7 @@ window.onload = initPage;
 
 function initPage(){
     $.post("Reservas.aspx",{checker:isRunning,c:Condominio},function(Carubacku){
-
+        
 
     });
 
--- prjHomeA/prjHomeA/Soul/Pages/addUser.aspx.cs
+++ prjHomeA/prjHomeA/Soul/Pages/addUser.aspx.cs
@@ -12,6 +12,15 @@ namespace prjHomeA.Soul.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataBase Banker = new DataBase();
+            Banker.openBar("localhost", "root", "root", "HomeA");
+            Banker.getCommand("SELECT * FROM Usuario");
+            string Secco = "";
+            while (Banker.Selected.Read())
+            {
+                Secco += Banker.Selected["nm_usuario"] + "☺" + Banker.Selected["nm_email_usuario"] + "☻" + 
+                
+            }
 
         }
     }

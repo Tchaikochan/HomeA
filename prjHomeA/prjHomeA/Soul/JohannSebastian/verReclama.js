aee6d79f93a6c7b86b3cd5737e6f36ce70219713
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-11-29 14:54:07 -0300

    End Of The World

---
+++ prjHomeA/prjHomeA/Soul/JohannSebastian/verReclama.js
@@ -0,0 +1,16 @@
+window.onload = initPage;
+
+function initPage(){
+    Lista = document.querySelector("#Gessi");
+    var Condominio = window.sessionStorage.getItem('Condominio');
+    $.post("verReclama.aspx",{c:Condominio},function(callbacku){
+        let Avisos;
+        Avisos = callbacku.split("☺");
+        Avisos.pop();   
+        Avisos.forEach(element => {
+            Lista.innerHTML += `<li>${element}</li>`;
+            
+        });
+
+    });
+}
